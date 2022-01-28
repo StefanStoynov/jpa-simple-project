a30be95885f0b05eab7ade51523456884c67140f
StefanStoynov <sstoynov87@gmail.com>
2022-01-28 20:41:11 +0200

    Initial commit

---
+++ src/main/java/com/ss/jpasimpleproject/JpaSimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ss.jpasimpleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSimpleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSimpleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss/jpasimpleproject/JpaSimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.jpasimpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSimpleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
